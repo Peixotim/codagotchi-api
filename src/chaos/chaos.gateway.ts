9059eb7a80d4d9121379ae42e1e8303829823058
Peixotim <pedropeixotovz@gmail.com>
2025-12-01 17:03:13 -0300

    feat: refatorar ChaosGateway para incluir gerenciamento de conexão e desconexão de clientes

--- src/chaos/chaos.gateway.ts
+++ src/chaos/chaos.gateway.ts
@@ -1,11 +1,51 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: { origin: '*' },
-})
-export class ChaosGateway {
+  namespace: 'chaos',
+}) //Controller do websockets
+export class ChaosGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return `${client} ,${payload}`;
+  public testandoSocket(
+    @ConnectedSocket() user: Socket,
+    @MessageBody() data: string,
+  ) {
+    console.log({
+      user: user.id,
+      data: data,
+    });
+    return {
+      user: user.id,
+      data: data,
+    };
   }
+
+  public handleConnection(user: Socket) {
+    console.log(`Client connected: ${user.id}`);
+    user.emit('connection', { message: 'Connected to chaos namespace' });
+  } //Fala o user que foi conectado
+
+  public handleDisconnect(user: Socket) {
+    console.log(`Client Disconnect : ${user.id}`);
+    user.emit('connection', { message: 'Connected to chaos namespace' });
+  }
+
+  public afterInit() {
+    console.log('WebSocket Sendo Iniciado ...');
+  } // Fala sobre a inicialização do WS
 }
