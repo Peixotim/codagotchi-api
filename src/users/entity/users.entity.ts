3c14abf37d1351bd7b8e23e9ea0eafbc9ab94314
Peixotim <pedropeixotovz@gmail.com>
2025-12-01 17:23:15 -0300

    feat: adicionar entidade UsersEntity para gerenciamento de usuários

---
+++ src/users/entity/users.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UsersEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  mail: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
