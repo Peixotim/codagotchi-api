cf79ceac59c2944152d066ab46a51dd8b76d9d13
Peixotim <pedropeixotovz@gmail.com>
2025-12-01 14:58:31 -0300

    feat: adicionar ChaosGateway como provedor no módulo Chaos

--- src/chaos/chaos.module.ts
+++ src/chaos/chaos.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChaosGateway } from './chaos.gateway';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [ChaosGateway],
 })
 export class ChaosModule {}
