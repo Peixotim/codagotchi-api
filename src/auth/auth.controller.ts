dcae5372a86bb8a1ab183c502b8d8ad196c8aea6
Peixotim <pedropeixotovz@gmail.com>
2025-12-01 22:16:38 -0300

    feat: implementar controlador de autenticação com métodos de registro e login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthLoginDTO } from './DTOs/auth-login.dto';
+import { UsersCreate } from 'src/users/DTOs/users-create.dto';
+import { UsersEntity } from 'src/users/entity/users.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  public async register(request: UsersCreate): Promise<UsersEntity> {
+    return await this.authService.registerUser(request);
+  }
+
+  @Post('login')
+  public async login(request: AuthLoginDTO): Promise<{ acess_token }> {
+    return await this.authService.loginUser(request);
+  }
+}
