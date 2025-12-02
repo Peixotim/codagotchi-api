98e966d0e4e57331c6f1bfa0d2174db055c7663b
Peixotim <pedropeixotovz@gmail.com>
2025-12-01 22:16:41 -0300

    feat: configurar módulo de autenticação com provedores e controladores

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'admin',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  exports: [AuthService],
+})
 export class AuthModule {}
